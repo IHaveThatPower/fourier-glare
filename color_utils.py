dc4b547763da506e2cbc04a999adf18c2b79d357
Ryan McClure <mcc@mcc3d.com>
2025-11-02 10:51:48 -0500

    Removed unused wavelength-to-XYZ CMF approaches. Added some additional pre-made apertures.

--- color_utils.py
+++ color_utils.py
@@ -5,30 +5,6 @@ import numpy, cv2
 class ColorUtils:
 	cmf = {}
 
-	# Constant parameters for the piecewise Gaussian functions
-	# used in wavelength-to-XYZ color conversion
-	cie_g = {
-		"x": [
-			[599.8, 0.0264, 0.0323],
-			[442., 0.0624, 0.0374],
-			[501.1, 0.0490, 0.0382]
-		],
-		"y": [
-			[568.8, 0.0213, 0.0247],
-			[530.9, 0.0613, 0.0322]
-		],
-		"z": [
-			[437., 0.0845, 0.0278],
-			[459., 0.0385, 0.0725]
-		]
-	}
-	# Scalar parameters for the results of the Gaussian functions
-	cie_scalar = {
-		"x": [1.056, 0.362, -0.065],
-		"y": [0.821, 0.286],
-		"z": [1.217, 0.681]
-	}
-
 	@staticmethod
 	def get_rgb_from_wavelength(l, fake_image):
 		"""
@@ -69,21 +45,8 @@ class ColorUtils:
 		list
 			a list of XYZ values corresponding to the given wavelength
 		"""
-		# result = {}
-		# for axis in ["x", "y", "z"]:
-		# 	# Collect computed t_params
-		# 	t = [ColorUtils.t_param(axis, l, t_n) for t_n in range(0, len(ColorUtils.cie_g[axis]))]
-		# 	result[axis] = ColorUtils.xyz_part(axis, l, *t)
-		# x, y, z = itemgetter('x', 'y', 'z')(result)
-		# return [x, y, z]
-		# print("NEW\t%s\t%s" % (l, [x, y, z]))
-		# old = ColorUtils.lamba_to_xyz_old(l)
-		# return old
-		# print("OLD\t%s\t%s" % (l, old))
 		if (len(ColorUtils.cmf.keys()) == 0):
 			ColorUtils.loadCMF()
-		# print("REF\t%s\t%s" % (l, cmf[l]))
-		# exit()
 		return ColorUtils.cmf[l]
 
 	@staticmethod
@@ -100,122 +63,6 @@ class ColorUtils:
 			if line == "": continue # Skip empty strings
 			line_data = [numpy.float64(l) for l in line.split("\t")]
 			ColorUtils.cmf[int(line_data[0])] = list(line_data[1:])
-
-	@staticmethod
-	def t_param(axis, l, t):
-		"""
-		Compute a partial component of the matching function
-
-		Parameters
-		----------
-		axis : string
-			The axis (X, Y, Z)
-		l : float
-			The wavelength
-		t : int
-			The parameter being computed
-
-		Returns
-		-------
-		float
-			The computed t parameter
-		"""
-		ref_l = ColorUtils.cie_g[axis][t][0]
-		t_val = ColorUtils.cie_g[axis][t][1] if l < ref_l else ColorUtils.cie_g[axis][t][2]
-		t_val *= (l - ref_l)
-		return t_val
-
-	@staticmethod
-	def xyz_part(axis, l, t1, t2, t3=None):
-		part_val = 0
-		t = [t1, t2, t3]
-		for i in range(0, len(t)):
-			if t[i] is None:
-				continue
-			part_val += ColorUtils.cie_scalar[axis][i] * numpy.exp(-0.5 * t[i] * t[i])
-		return part_val
-
-	# DEPRECATED
-	# Constant parameters for the piecewise Gaussian functions
-	# used in wavelength-to-XYZ color conversion
-	cie_g_old = {
-		"x": [
-			[599.8, 37.9, 31.0],
-			[442., 16., 26.7],
-			[501.1, 20.4, 26.2]
-		],
-		"y": [
-			[568.8, 46.9, 40.5],
-			[530.9, 16.3, 31.1]
-		],
-		"z": [
-			[437., 11.8, 36.],
-			[459., 26., 13.8]
-		]
-	}
-	# Scalar parameters for the results of the Gaussian functions
-	cie_scalar_old = {
-		"x": [1.056, 0.362, -0.065],
-		"y": [0.821, 0.286],
-		"z": [1.217, 0.681]
-	}
-
-	# DEPRECATED
-	@staticmethod
-	def cie_gaussian_old(l, mu, s_1, s_2):
-		"""
-		Given a wavelength, mu, sigma_1 and sigma_2, return the result
-		of the gaussian function.
-
-		Parameters
-		----------
-		l : int
-			The wavelength
-		mu : float
-			The mu value of the gaussian function
-		s_1 : float
-			The sigma_1 value of the gaussian function
-		s_2 : float
-			The sigma_2 value of the gaussian function
-
-		Returns
-		-------
-		float
-			The computed gaussian function
-		"""
-		l_mu = -0.5 * (l - mu)**2
-		if (l < mu):
-			return numpy.exp(l_mu / s_1**2)
-		else:
-			return numpy.exp(l_mu / s_2**2)
-
-	# DEPRECATED
-	@staticmethod
-	def lamba_to_xyz_old(l):
-		"""
-		Given a wavelength, convert it to XYZ colorspace.
-
-		Parameters
-		----------
-		l : float
-			The wavelength
-
-		Returns
-		-------
-		list
-			a list of XYZ values corresponding to the given wavelength
-		"""
-		x = \
-				ColorUtils.cie_scalar_old["x"][0] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["x"][0]) + \
-				ColorUtils.cie_scalar_old["x"][1] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["x"][1]) + \
-				ColorUtils.cie_scalar_old["x"][2] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["x"][2])
-		y = \
-				ColorUtils.cie_scalar_old["y"][0] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["y"][0]) + \
-				ColorUtils.cie_scalar_old["y"][1] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["y"][1])
-		z = \
-				ColorUtils.cie_scalar_old["z"][0] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["z"][0]) + \
-				ColorUtils.cie_scalar_old["z"][1] * ColorUtils.cie_gaussian_old(l, *ColorUtils.cie_g_old["z"][1])
-		return [x, y, z]
 
 	# CURRENTLY UNUSED
 	@staticmethod
